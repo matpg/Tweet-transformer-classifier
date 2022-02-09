0059df68e5bc77007c1df2df6ba4e051dc3176b4
PoBLETS <matpoga@gmail.com>
2022-02-09 20:26:04 -0300

    Clasificador Transformer añadido

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+# title and description
+st.write("""
+# Tweet Transformer Classifier
+Write your Tweet here:
+""")
+
+# search bar
+query = st.text_input("Classify!", "")
+
+labels = ['agradecimiento', 'consulta', 'fraude', 'otro', 'problema',  'reclamo']
+
+access_token = os.getenv('ACCESS_TOKEN')
+
+MODEL_NAME = os.getenv('MODEL_NAME')
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_auth_token=access_token)
+
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, use_auth_token=access_token)
+
+if query != "":
+    inputs = tokenizer(query, return_tensors="pt")
+    outputs = model(**inputs)
+    response = outputs.logits 
+    response = response.detach().numpy()
+    response = response.tolist()
+    response = response[0]
+    response = response.index(max(response))
+    response = labels[response]
+    st.write(f"Label: {response}")
