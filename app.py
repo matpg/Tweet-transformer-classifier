4713aff746aac793896c45341afaee81c8c492c5
PoBLETS <matpoga@gmail.com>
2022-02-10 10:18:23 -0300

    Cache decorator added

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # title and description
@@ -10,15 +14,24 @@ Write your Tweet here:
 # search bar
 query = st.text_input("Classify!", "")
 
+@st.experimental_singleton
+def credentials():
+    ACCES_TOKEN = st.secrets["ACCESS_TOKEN"]
+    MODEL_NAME =  st.secrets["MODEL_NAME"]
+    return ACCES_TOKEN, MODEL_NAME
+
+
 labels = ['agradecimiento', 'consulta', 'fraude', 'otro', 'problema',  'reclamo']
 
-access_token = st.secrets["ACCESS_TOKEN"]
+access_token, model_name = credentials()
 
-MODEL_NAME = st.secrets["MODEL_NAME"]
+@st.experimental_singleton
+def transformer_model(access_token, model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name, user_token=access_token)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name,  user_token=access_token)
+    return tokenizer, model
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_auth_token=access_token)
-
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, use_auth_token=access_token)
+tokenizer, model = transformer_model()
 
 if query != "":
     inputs = tokenizer(query, return_tensors="pt")
